bb9c9f2c9b5ad53617ad2238a3aa3f3c4b6ca7a1
westofsky <westofsky159@gmail.com>
2023-03-31 14:41:12 +0900

    style : Input 레이아웃 작성

---
+++ src/components/ChatInput.tsx
@@ -0,0 +1,16 @@
+import {atom, useRecoilState } from 'recoil';
+import {ChatInputWrapper, Input, SubmitButton} from '../styles/style.chatinput';
+import {FaTelegramPlane} from 'react-icons/fa';
+function ChatInput() : JSX.Element {
+  
+  return (
+    <ChatInputWrapper>
+      <Input
+        placeholder="메세지를 입력해주세요"
+      />
+      <SubmitButton><FaTelegramPlane/></SubmitButton>
+    </ChatInputWrapper>
+  )
+}
+
+export default ChatInput;
---
+++ src/styles/style.chatinput.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const ChatInputWrapper = styled.form`
+    margin : 0px 10px 10px;
+    display : flex;
+`
+
+export const Input = styled.input`
+    width:100%;
+    border: none;
+    font-size: 1.2rem;
+    line-height: 1.5rem;
+`
+
+export const SubmitButton = styled.button`
+    padding: 10px;
+    font-size: 30px;
+    cursor: pointer;
+    border : none;
+`
