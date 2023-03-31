e64e76af79cf12bfe72bfb2879ce03610904a7e2
Sujin <shtnwls1111@ewhain.net>
2023-03-31 23:42:28 +0900

    user 바꾸기

---
+++ src/UserList.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import { User } from './Interfaces';
+import UserItem from './UserItem';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 5px;
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 20px;
+  height: 10%;
+  box-sizing: border-box;
+`;
+
+interface UserListProps {
+  userId: number;
+  users: User[];
+  changeUser(id: number): void;
+}
+
+const UserList = ({ userId, users, changeUser }: UserListProps) => {
+  return (
+    <Wrapper>
+      {users.map((user) => (
+        <UserItem
+          key={user.id}
+          selected={user.id === userId}
+          user={user}
+          changeUser={changeUser}
+        />
+      ))}
+    </Wrapper>
+  );
+};
+
+export default UserList;
