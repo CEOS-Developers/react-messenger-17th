3d9deac208420b1fb31b6daba173a843e7f29fcd
flowersayo <flowersayo0811@ewhain.net>
2023-03-31 18:03:07 +0900

    feat: signup

--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Routes, Route, Navigate } from 'react-router-dom';
-//import loadable from '@loadable/component';
+//import loadable from '@loadable/component'; -> 왜인지 버전오류..
 import LogIn from 'src/pages/Login';
 import SignUp from 'src/pages/SignUp';
 //const LogIn = loadable(() => import('../pages/Login'));
@@ -12,6 +12,7 @@ export default function App() {
       <Route path="/login" element={<LogIn />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="*" element={<Navigate to="/" replace />} />
+      {/*매칭이 되지 않는 url 들어오면 루트경로로 Redirect*/}
     </Routes>
   );
 }
