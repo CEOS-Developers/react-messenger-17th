c92c797f1cd16705899b520382502b6eda07369c
flowersayo <flowersayo0811@ewhain.net>
2023-03-31 21:43:43 +0900

    feat: add recoil

--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
-
+import DirectMessage from '../pages/DirectMessage';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import Channel from 'src/pages/Channel';
 //import loadable from '@loadable/component'; -> 왜인지 버전오류..
 import LogIn from 'src/pages/Login';
 import SignUp from 'src/pages/SignUp';
 //const LogIn = loadable(() => import('../pages/Login'));
 //const SignUp = loadable(() => import('../pages/SignUp'));
+// 코드 스플리팅 !!
+
+import { RecoilRoot, atom, selector, useRecoilState, useRecoilValue } from 'recoil';
 export default function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LogIn />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-      {/*매칭이 되지 않는 url 들어오면 루트경로로 Redirect*/}
-    </Routes>
+    <RecoilRoot>
+      <Routes>
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+        {/*매칭이 되지 않는 url 들어오면 루트경로로 Redirect*/}
+      </Routes>
+    </RecoilRoot>
   );
 }
