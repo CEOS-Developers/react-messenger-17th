df42264ed05d5a767b2d3d66be00296b3ec1f3c1
westofsky <westofsky159@gmail.com>
2023-03-31 16:49:13 +0900

    feat : 입력 시 메세지 전송 기능

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -2,17 +2,37 @@ import {useRecoilState } from 'recoil';
 import {useState} from 'react';
 import {ChatInputWrapper, Input, SubmitButton} from '../styles/style.chatinput';
 import {FaTelegramPlane} from 'react-icons/fa';
+import {IChatRoom} from '../store/interface';
+import {roomList,userInfo} from '../store/atom';
+
 function ChatInput() : JSX.Element {
-  
+  const [currentUser, setCurrentUser] = useRecoilState(userInfo);
+  const [roomLists, setRoomLists] = useRecoilState<IChatRoom[]>(roomList);
   const [inputValue, setInputValue] = useState("");
+  const roomIndex = roomLists.findIndex((room) => room.roomid === 1);
+  
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
   };
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if(!inputValue.trim())
-        return;
-        
+      return;
+    const newMessage = {
+        id : Date.now(),
+        userid : currentUser.userid,
+        message : inputValue
+    };
+    const newRoom = {
+        ...roomLists[roomIndex],
+        messages : [...roomLists[roomIndex].messages, newMessage],
+    };
+    
+    setRoomLists([
+        ...roomLists.slice(0,roomIndex),
+        newRoom,
+        ...roomLists.slice(roomIndex+1),
+    ]);
     setInputValue("");
   };
   return (
