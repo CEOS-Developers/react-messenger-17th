6454a1c5ee618a21c82b0ffc8fca720bbc551001
westofsky <westofsky159@gmail.com>
2023-03-31 16:49:51 +0900

    feat : 입력 시 scroll 기능 추가

--- src/components/ChatContent.tsx
+++ src/components/ChatContent.tsx
@@ -1,13 +1,24 @@
 import {useRecoilState } from 'recoil';
 import { ChatContentWrapper } from '../styles/style.chatcontent';
-import {roomList} from '../store/atom';
+import {roomList,userInfo} from '../store/atom';
 import ChatItem from './ChatItem';
+import {useRef,useEffect} from 'react';
 function ChatContent(): JSX.Element {
   const [roomLists, setChatLists] = useRecoilState(roomList);
+  const [currentUser, setCurrentUser] = useRecoilState(userInfo);
   const chatList = roomLists[0].messages;
+  const chatContent = useRef<HTMLDivElement>(null);
   
+  const scrollChat = () => {
+    if (chatContent.current)
+      chatContent.current.scrollTop = chatContent.current.scrollHeight;
+  }
+  
+  useEffect(() => {
+    scrollChat();
+  },[roomLists,userInfo])
   return (
-    <ChatContentWrapper>
+    <ChatContentWrapper ref = {chatContent}>
       {chatList.map((item) => (
         <ChatItem
           key={item.id}
