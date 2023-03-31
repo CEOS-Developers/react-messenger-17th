4c7fd5abf8f7b15f697e07e8c33c8c930558cb56
권가은 <aileen1007@ewhain.net>
2023-04-01 02:06:01 +0900

    fix: getDateString 함수 수정

--- src/utils/getDateString.ts
+++ src/utils/getDateString.ts
@@ -1,17 +1,20 @@
 export const getDateString = (date: string) => {
-  const dateTypeofDate = new Date(date);
+  const dateStrToDate = new Date(date);
 
   const today = new Date();
   const isToday =
-    dateTypeofDate.getFullYear() === today.getFullYear() &&
-    dateTypeofDate.getMonth() === today.getMonth() &&
-    dateTypeofDate.getDate() === today.getDate();
+    dateStrToDate.getFullYear() === today.getFullYear() &&
+    dateStrToDate.getMonth() === today.getMonth() &&
+    dateStrToDate.getDate() === today.getDate();
 
-  const hours: number = dateTypeofDate.getHours();
+  const hours: number = dateStrToDate.getHours();
   const hours12: number = hours <= 12 ? hours : hours - 12;
-  const minutes: string = String(dateTypeofDate.getMinutes()).padStart(2, "0");
+  const minutes: string = String(dateStrToDate.getMinutes()).padStart(2, "0");
   const amPm: string = hours <= 12 ? "오전" : "오후";
+
   return isToday
     ? `${amPm} ${hours12}:${minutes}`
-    : `${today.getMonth()}월 ${String(today.getDate()).padStart(2, "0")}일`;
+    : `${dateStrToDate.getMonth() + 1}월 ${String(
+        dateStrToDate.getDate()
+      ).padStart(2, "0")}일`;
 };
