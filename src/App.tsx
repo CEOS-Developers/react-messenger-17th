37dd69006f0089b28e8ebacdbc1aca753bc2ac64
Yoojin Shin <yjshin1003@naver.com>
2023-04-30 20:35:20 +0900

    feat: 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
+import { Outlet } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import styled from 'styled-components';
 import GlobalStyle from './styles/GlobalStyles';
-import ChatRoomPage from './components/ChatRoomPage/ChatRoomPage';
 
 function App() {
   return (
     <RecoilRoot>
       <GlobalStyle />
       <Container>
-        <ChatRoomPage />
+        <Outlet />
       </Container>
     </RecoilRoot>
   );
@@ -20,12 +20,14 @@ const Container = styled.div`
   width: 26rem;
   height: 52rem;
 
+  /*
   background: rgb(126, 246, 247);
   background: linear-gradient(
     0deg,
     rgba(126, 246, 247, 1) 0%,
     rgba(167, 80, 255, 1) 86%
   );
+  */
   border-radius: 2.3rem;
   box-shadow: 0px 0px 5px #444;
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import FriendsPage from './components/FriendsPage/FriendsPage';
+import ChatRoomListPage from './components/ChatRoomListPage/ChatRoomListPage';
+import ChatRoomPage from './components/ChatRoomPage/ChatRoomPage';
+import SettingPage from './components/SettingPage/SettingPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <FriendsPage /> },
+      { path: 'friends', element: <FriendsPage /> },
+      { path: 'chatrooms', element: <ChatRoomListPage /> },
+      { path: 'chatrooms/:roomId', element: <ChatRoomPage /> },
+      { path: 'settings', element: <SettingPage /> },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
