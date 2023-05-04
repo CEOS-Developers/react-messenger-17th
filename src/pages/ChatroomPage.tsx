d78ad09f91f98c3c94ecda365bfde667b9ec98ed
Yelynn Oh <yoyelynn@gmail.com>
2023-05-05 08:44:09 +0900

    feat: 오래된 순 기능 구현 & 호버 효과 추가

--- src/pages/ChatroomPage.tsx
+++ src/pages/ChatroomPage.tsx
@@ -6,12 +6,13 @@ import { getRoomMembers } from "../common/Custom";
 import { ProfileImage } from "../common/StyledComponent";
 import styled from "styled-components";
 import { useState } from "react";
+import LatestMsg from "../components/ChatRoomPage/LatestMsg";
 
 const ChatroomPage = () => {
 
   //sort to the oldest
   const [sort, setSort] = useState(false);
-  const chatrooms = sort ? [...chatData.rooms].reverse() : chatData.rooms;
+  const chatrooms = sort ? chatData.rooms : [...chatData.rooms].reverse();
   const handleSort = () => {
       setSort(!sort);
   };
@@ -21,24 +22,31 @@ const ChatroomPage = () => {
       <Header>
         <Title>
           <span>채팅</span>
-          <div> 
-            <span>오래된 순</span>
-            <SortBox type="checkbox" onChange={handleSort} checked={sort}></SortBox>
-          </div>
         </Title>
+        <div style={{display: "flex", flexDirection: "row", gap: "4px", paddingTop: "1rem", paddingLeft: "1rem"}}> 
+            <h3>오래된 순</h3>
+            <ToggleWrapper>
+              <CheckBox
+                  type="checkbox"
+                  onChange={handleSort}
+              />
+              <LeftSide isChecked={sort}></LeftSide>
+              <RightSide isChecked={!sort}></RightSide>
+            </ToggleWrapper>
+        </div>
       </Header>
 
       <Body> 
             {chatrooms.map(room => (
-              <Link to={`/chatrooms/${room.roomId}`}>
+              <Link to={`/chatrooms/${room.roomId}`} style={{ textDecoration: "none", color: "black" }}>
                 <div>
                   {getRoomMembers(room.roomId, false).map((member) => (
-                  <div>
-                  <ProfileImage src={`${member.profileImage}`} />
-                  <ChatInfo>
-                    <h3> {member.name} </h3>
-                  </ChatInfo>
-                  </div>
+                  <ChatBox>
+                    <ProfileImg><img src={`${member.profileImage}`}></img></ProfileImg>
+                    <div>
+                      <LatestMsg userName={member.name} chatMessage={room.chats[1].text} timestamp={room.chats[1].date}/>
+                    </div>
+                  </ChatBox>
                 ))}
                 </div>
               </Link> 
@@ -60,9 +68,6 @@ const Wrapper = styled.div`
 
 const Title = styled.h1`
   font-size: 1.5rem;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
   padding-top: 1rem;
   padding-left: 1rem;
 `;
@@ -70,19 +75,26 @@ const Title = styled.h1`
 const Header = styled.div`
   gap: 0.5rem;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 const Body = styled.div`
   display: flex;
   flex-direction: column;
-  // align-items: center;
+  justify-items: center;
   height: 5rem;
-  gap: 15px;
 `;
 
-const SortBox = styled.input`
-
+const ChatBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items:center;
+  height: 5rem;
+  :hover {
+    background-color: #ffecd8;
+  }
 `;
 
 const ProfileImg = styled.div`
@@ -93,15 +105,40 @@ const ProfileImg = styled.div`
     width: 3rem;
     height: 3rem;
     border-radius: 1rem;
-    border: 2px solid #D9D9D9;
     cursor: pointer;
   }
 `;
 
-const ChatInfo = styled.div`
-
-`;
-
-const RecentChat = styled.div`
-
-`;
+const ToggleWrapper = styled.div`
+    border-radius: 2em;
+    width: 60px;
+    height: 30px;
+    position: relative;
+    background-color: #EEEBEB;
+`
+const LeftSide = styled.div<{ isChecked: boolean }>`
+    cursor: pointer;
+    position: relative; 
+`
+const RightSide = styled.div<{ isChecked: boolean }>`
+    cursor: pointer;
+    position: relative;
+`
+const CheckBox = styled.input`
+    // display: none;
+    cursor: pointer;
+    width: 1rem;
+    height: 1rem;
+    position: absolute;
+    border-radius: 50%;
+    left: 5px;
+    top: 4px;
+    transition: all 0.2s ease-in-out;
+    :checked{
+      width: 1rem;
+      height: 1rem;
+      position: absolute;
+      left: 28px;
+      transition: all 0.2s ease-in-out;
+    }
+    `;
