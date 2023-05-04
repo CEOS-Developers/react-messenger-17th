1133ac724fa737fd31e2c0cc0ffd63f31f725541
Yoojin Shin <yjshin1003@naver.com>
2023-05-05 08:48:35 +0900

    feat: FriendsPage에서 친구 검색 기능 구현

--- src/components/FriendsPage/FriendsPage.tsx
+++ src/components/FriendsPage/FriendsPage.tsx
@@ -1,12 +1,48 @@
-import React from 'react';
-import Header from './Header';
+import React, { useState } from 'react';
 import ProfileList from './ProfileList';
+import { useRecoilValue } from 'recoil';
+import { friendsInfoSelector } from '../../state/atom';
+import Profile from './Profile';
 
 const FriendsPage = () => {
+  const [isSearching, setIsSearching] = useState(false);
+  const [text, setText] = useState('');
+  const friendsInfo = useRecoilValue(friendsInfoSelector);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
   return (
     <>
-      <Header />
-      <ProfileList />
+      <header>
+        <h1>친구</h1>
+        <button onClick={() => setIsSearching(!isSearching)}>🔍</button>
+      </header>
+
+      {isSearching ? (
+        <>
+          <input placeholder="친구 검색" value={text} onChange={handleChange} />
+          <>
+            {friendsInfo.map((friend) => (
+              <>
+                {friend.userName.includes(text) ? (
+                  <Profile
+                    key={friend.userId}
+                    userName={friend.userName}
+                    userImg={friend.userImg}
+                    statusMessage={friend.statusMessage}
+                  />
+                ) : (
+                  <></>
+                )}
+              </>
+            ))}
+          </>
+        </>
+      ) : (
+        <ProfileList />
+      )}
     </>
   );
 };
