a1ae36ad0cbe34eab9f3076cc54886171d7666bd
Yoojin Shin <yjshin1003@naver.com>
2023-05-04 16:39:04 +0900

    feat: ChatRoomList 컴포넌트에서 ChatRoom 컴포넌트에 마지막 채팅이 아닌 모든 채팅 전달로 수정

--- src/components/ChatRoomListPage/ChatRoom.tsx
+++ src/components/ChatRoomListPage/ChatRoom.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
+import { IChat } from '../../interface/interface';
 
 type ChatRoomProps = {
   userName: string;
   userImg: string;
-  lastMessage: string;
-  lastTime: string;
+  chatList: IChat[];
 };
-const ChatRoom = ({
-  userName,
-  userImg,
-  lastMessage,
-  lastTime,
-}: ChatRoomProps) => {
+
+const ChatRoom = ({ userName, userImg, chatList }: ChatRoomProps) => {
+  const lastChat = chatList[chatList.length! - 1];
+
   return (
     <div>
       <p>{userName}</p>
-      <p>{lastMessage}</p>
-      <p>{lastTime}</p>
+      <p>{lastChat.content}</p>
+      <p>{lastChat.time}</p>
     </div>
   );
 };
--- src/components/ChatRoomListPage/ChatRoomList.tsx
+++ src/components/ChatRoomListPage/ChatRoomList.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useRecoilValue } from 'recoil';
 import { allChatRoomsInfoState, friendsInfoSelector } from '../../state/atom';
 import ChatRoom from './ChatRoom';
-import { IChatRoom } from '../../interface/interface';
 
 const ChatRoomList = () => {
   const allChatRoomsInfo = useRecoilValue(allChatRoomsInfoState);
@@ -11,21 +10,19 @@ const ChatRoomList = () => {
   return (
     <div>
       <ul>
-        {allChatRoomsInfo.map((chatRoomInfo: IChatRoom) => {
+        {allChatRoomsInfo.map((chatRoomInfo) => {
           const userId = chatRoomInfo.userId; //채팅방 정보 안의 userId
           const user = friendsInfo.find((friend) => friend.userId === userId);
           const userName = user?.userName!;
           const userImg = user?.userImg!;
-          const lastChat =
-            chatRoomInfo.chatList[chatRoomInfo.chatList.length! - 1];
+          const chatList = chatRoomInfo.chatList;
 
           return (
             <ChatRoom
               key={userId}
               userName={userName}
               userImg={userImg}
-              lastMessage={lastChat?.content}
-              lastTime={lastChat?.time}
+              chatList={chatList}
             />
           );
         })}
