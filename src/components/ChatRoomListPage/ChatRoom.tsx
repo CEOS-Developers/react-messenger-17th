7f56ff2905fbb0396fe66c646dc235fcf877e502
Yoojin Shin <yjshin1003@naver.com>
2023-05-05 17:38:49 +0900

    style: ChatRoomListPage 스타일링

--- src/components/ChatRoomListPage/ChatRoom.tsx
+++ src/components/ChatRoomListPage/ChatRoom.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { IChat, IUser } from '../../interface/interface';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 type ChatRoomProps = {
   friendInfo: IUser;
@@ -12,17 +13,71 @@ const ChatRoom = ({ friendInfo, chatList }: ChatRoomProps) => {
   const lastChat = chatList[chatList.length! - 1];
 
   return (
-    <div
+    <ChatRoomBtn
       onClick={() => {
         navigate(`/chatrooms/${friendInfo.userId}`);
       }}
     >
-      <p>{friendInfo.userName}</p>
-      {/* {friendInfo.userImg} */}
-      <p>{lastChat.content}</p>
-      <p>{lastChat.time}</p>
-    </div>
+      <LeftBox>
+        <UserImg
+          src={`${process.env.PUBLIC_URL}/Imgs/${friendInfo.userImg}.jpg`}
+          alt={friendInfo.userName}
+        />
+        <div>
+          <UserName>{friendInfo.userName}</UserName>
+          <LastContent>{lastChat.content}</LastContent>
+        </div>
+      </LeftBox>
+      <RightBox>
+        <LastTime>{lastChat.time}</LastTime>
+      </RightBox>
+    </ChatRoomBtn>
   );
 };
 
 export default ChatRoom;
+
+const ChatRoomBtn = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem 1.5rem;
+  cursor: pointer;
+  background-color: transparent;
+  transition: all 100ms ease-out;
+
+  &:hover {
+    background-color: #a6a4a321;
+  }
+`;
+
+const UserImg = styled.img`
+  width: 3.5rem;
+  height: 3.5rem;
+  border: 0.15rem solid #b8b6b6;
+  border-radius: 50%;
+  margin-right: 1rem;
+`;
+
+const UserName = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  font-weight: bold;
+  font-family: 'IBMPlexSansKR-Regular';
+`;
+
+const LastContent = styled.div`
+  font-family: 'IBMPlexSansKR-Regular';
+`;
+
+const LeftBox = styled.div`
+  display: flex;
+`;
+
+const RightBox = styled.div`
+  margin-left: 0.5rem;
+`;
+
+const LastTime = styled.div`
+  font-size: 0.75rem;
+  font-family: 'IBMPlexSansKR-Regular';
+`;
--- src/components/ChatRoomListPage/ChatRoomListPage.tsx
+++ src/components/ChatRoomListPage/ChatRoomListPage.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import Header from './Header';
 import ChatRoomList from './ChatRoomList';
+import styled from 'styled-components';
 
 const ChatRoomListPage = () => {
   return (
     <div>
-      <Header />
+      <Header>
+        <h1>채팅</h1>
+      </Header>
+
       <ChatRoomList />
     </div>
   );
 };
 
 export default ChatRoomListPage;
+
+const Header = styled.header`
+  font-family: 'IBMPlexSansKR-Regular';
+  padding: 0 1.5rem;
+`;
