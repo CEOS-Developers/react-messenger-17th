7db510cf517659b990df198cd67a08500e78c049
SujinRo <shtnwls1111@ewhain.net>
2023-05-05 15:41:14 +0900

    ChatRooms page 디자인적인 부분

--- src/pages/ChatRooms.tsx
+++ src/pages/ChatRooms.tsx
@@ -3,7 +3,7 @@ import Back from "../components/Bar/Back";
 import MenuBar from "../components/Bar/MenuBar";
 import chatData from "../jsons/chatData.json";
 import userData from "../jsons/userData.json";
-import {User} from "../interfaces/Interface";
+import {User, Chat} from "../interfaces/Interface";
 import { Link } from "react-router-dom";
 
 const Wrapper = styled.div`
@@ -28,13 +28,85 @@ border-radius: 20px 20px 0 0;
 align-items: center;
 `;
 
-const Content =styled.div`
+const Contents =styled.div`
 background: linear-gradient(#D0F8B7, white, white, white);
 display: flex;
 flex-direction: column;
 height: 78%;
-justify-content: center;
+overflow: auto;
+::-webkit-scrollbar {
+  width: 5px;
+}
+::-webkit-scrollbar-thumb {
+  border-radius: 2px;
+  background: lightgray;
+}
+`;
+
+const Content = styled.div`
+display: flex;
+flex-direction: row;
 align-items: center;
+:hover{
+  background: #E2E2E2;
+  cursor: pointer;
+}
+`;
+
+const Image = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 100%;
+  object-fit: cover;
+`;
+
+const Circle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 60px;
+  height: 60px;
+  border-radius: 100%;
+  margin: 10px;
+  margin-bottom: 6px;
+  margin-top: 6px;
+  background-color: white;
+  border: 0.01px solid gray;
+`;
+
+const Line = styled.hr`
+display: flex;
+justify-content: center;
+border: 0.01px solid lightgrey;
+width: 320px;
+`;
+
+const All = styled.div`
+display: flex;
+flex-direction: column;
+width: 70%;
+`
+
+const Title = styled.div`
+display: flex;
+flex-direction: row;
+align-items: center;
+`
+
+const Name = styled.div`
+font-size: 15.5px;
+font-weight:800;
+`;
+
+const Day = styled.div`
+margin-left: 5px;
+font-size: 10px;
+color: gray;
+`;
+
+const Text = styled.div`
+margin-top: 5px;
+font-size: 13px;
 `;
 
 function ChatRooms(){
@@ -42,6 +114,19 @@ function ChatRooms(){
   const users = userData.users;
   const me = userData.me;
   const all = me.concat(users);
+
+  const getDate = (chat: Chat[])=>{
+    const month = String(new Date(chat[chat.length -1].date).getMonth()).padStart(2, '0');
+    const day = String(new Date(chat[chat.length -1].date).getDay()).padStart(2, '0');
+
+    return month + "월 " + day + "일";
+  }
+
+  const getContent = (chat: Chat[])=>{
+    const content = chat[chat.length -1].text;
+
+    return content;
+  }
 
   const getRoomMember=(roomId: number, isCurUser: Boolean)=>{
     const roomMember: User[] = [];
@@ -59,17 +144,35 @@ function ChatRooms(){
           <Back/>
           <h2 style={{fontSize: '20px', fontWeight: 1000,  margin:0, marginLeft:'10px'}}>My Chats</h2>
           </Bar>
-          <Content>
+          <Contents>
             {chatRooms.map((room) => (
-              <Link to = {`/Chat/${room.roomId}`}>
-                <div>
+              <Link to = {`/Chat/${room.roomId}`} style={{textDecoration: 'none', color: 'inherit'}}>
+                <Content>
                   {getRoomMember(room.roomId, false).map((member) => (
-                  <span>{member.name}</span>
-            ))}
-          </div>
-              </Link>
+                  <>
+                  <Circle>
+                  <Image src={member.image}></Image>
+                  </Circle>
+                  <All>
+                  <Title>
+                  <Name>
+                  {member.name}
+                  </Name>
+                  <Day>
+                  {getDate(room.chats)}
+                  </Day>
+                  </Title>
+                  <Text>
+                  {getContent(room.chats)}
+                  </Text>
+                  </All>
+                  </>
             ))}
           </Content>
+          <Line/>
+              </Link>
+            ))}
+          </Contents>
           <MenuBar/>
         </Wrapper>
     );
